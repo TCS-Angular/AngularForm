5037bd92169751369593e40f96845d6e9198b153
ngactive99 <ngactive99@gmail.com>
2022-04-12 17:56:39 +0530

    exxplored more pre built pipes

--- src/app/pipedemo/pipedemo.component.ts
+++ src/app/pipedemo/pipedemo.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-pipedemo',
   templateUrl: './pipedemo.component.html',
-  styleUrls: ['./pipedemo.component.css']
+  styleUrls: ['./pipedemo.component.css'],
 })
 export class PipedemoComponent implements OnInit {
-  myincome = 1000; // to display currency pipe
-  presentDate= new Date();
-  constructor() { }
+  timeChange = new Observable<String>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
 
-  ngOnInit(): void {
+  cars = ["Hyundai", "Mahindra", "Ford", "Mercedes", "Tata"]
+
+  num1 : number = 76.123123123;
+  num2: number = 0.676544345;
+
+  myinfo = {
+    name: "Nishant", phoneno: 556455678, address: { street: "10th strret", city: "Bangalore"}
   }
 
+  myincome = 1000; // to display currency pipe
+  presentDate = new Date();
+  constructor() {}
+
+  ngOnInit(): void {}
 }
